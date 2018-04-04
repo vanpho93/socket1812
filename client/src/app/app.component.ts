a3a8e6dbf4ddaad4bf2d6e79adaf5c32770e8fcd
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-04 19:45:38 +0700

    init socket.io

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import * as io from 'socket.io-client';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  constructor() {
+    const socket = io('http://localhost:3000');
+  }
+}
